d7a4fafb6a97233aebf8041c7207e2131c018a19
Emilie Rollandin <info@archistico.com>
2024-09-16 16:40:32 +0200

    Add files via upload

---
+++ NotifichePauseW/NotifichePauseW/frmConfigurazione.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace NotifichePauseW
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnTest = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nudMinuti = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbxTitolo = new System.Windows.Forms.TextBox();
+            this.tbxTesto = new System.Windows.Forms.TextBox();
+            this.btnSalva = new System.Windows.Forms.Button();
+            this.btnMinimizza = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.nudMinuti)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(12, 179);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(70, 33);
+            this.btnTest.TabIndex = 0;
+            this.btnTest.Text = "Prova";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(226, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Gestione notifiche a tempo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Minuti tra le notifiche";
+            // 
+            // nudMinuti
+            // 
+            this.nudMinuti.Location = new System.Drawing.Point(160, 42);
+            this.nudMinuti.Maximum = new decimal(new int[] {
+            480,
+            0,
+            0,
+            0});
+            this.nudMinuti.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudMinuti.Name = "nudMinuti";
+            this.nudMinuti.Size = new System.Drawing.Size(90, 20);
+            this.nudMinuti.TabIndex = 3;
+            this.nudMinuti.Value = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Titolo notifica";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Testo notifica";
+            // 
+            // tbxTitolo
+            // 
+            this.tbxTitolo.Location = new System.Drawing.Point(15, 87);
+            this.tbxTitolo.Name = "tbxTitolo";
+            this.tbxTitolo.Size = new System.Drawing.Size(235, 20);
+            this.tbxTitolo.TabIndex = 6;
+            this.tbxTitolo.Text = "Titolo";
+            // 
+            // tbxTesto
+            // 
+            this.tbxTesto.Location = new System.Drawing.Point(15, 138);
+            this.tbxTesto.Name = "tbxTesto";
+            this.tbxTesto.Size = new System.Drawing.Size(235, 20);
+            this.tbxTesto.TabIndex = 7;
+            this.tbxTesto.Text = "Testo notifica";
+            // 
+            // btnSalva
+            // 
+            this.btnSalva.Location = new System.Drawing.Point(110, 179);
+            this.btnSalva.Name = "btnSalva";
+            this.btnSalva.Size = new System.Drawing.Size(56, 33);
+            this.btnSalva.TabIndex = 8;
+            this.btnSalva.Text = "Salva";
+            this.btnSalva.UseVisualStyleBackColor = true;
+            this.btnSalva.Click += new System.EventHandler(this.btnSalva_Click);
+            // 
+            // btnMinimizza
+            // 
+            this.btnMinimizza.Location = new System.Drawing.Point(172, 179);
+            this.btnMinimizza.Name = "btnMinimizza";
+            this.btnMinimizza.Size = new System.Drawing.Size(78, 33);
+            this.btnMinimizza.TabIndex = 9;
+            this.btnMinimizza.Text = "Minimizza";
+            this.btnMinimizza.UseVisualStyleBackColor = true;
+            this.btnMinimizza.Click += new System.EventHandler(this.btnMinimizza_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(262, 230);
+            this.Controls.Add(this.btnMinimizza);
+            this.Controls.Add(this.btnSalva);
+            this.Controls.Add(this.tbxTesto);
+            this.Controls.Add(this.tbxTitolo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.nudMinuti);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnTest);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Notifiche";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.nudMinuti)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown nudMinuti;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbxTitolo;
+        private System.Windows.Forms.TextBox tbxTesto;
+        private System.Windows.Forms.Button btnSalva;
+        private System.Windows.Forms.Button btnMinimizza;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
